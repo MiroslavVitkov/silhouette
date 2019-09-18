81e53695aeec923d2c29fb5b593639499c8139b9
Miroslav Vitkov <sir.vorac@gmail.com>
2019-09-18 19:09:49 +0200

    [algo] Setting non-default metacoefficients to the HOG detector makes it finally useful!

--- src/cmd.cpp
+++ src/cmd.cpp
@@ -18,13 +18,20 @@ void CamDetectShow::execute()
     io::VideoPlayer player{ "faces" };
     cv::Mat frame;
     std::vector< cv::Rect > rects;
+    std::vector< double > weights;
 
     cv::HOGDescriptor detector;
     detector.setSVMDetector( cv::HOGDescriptor::getDefaultPeopleDetector() );
 
     while( cam >> frame )
     {
-        detector.detectMultiScale( frame, rects );
+        detector.detectMultiScale( frame
+                                 , rects
+                                 , weights
+                                 , 0
+                                 , cv::Size{ 4, 4 }
+                                 , cv::Size{ 8, 8 }
+                                 , 1.05 );
         io::draw_rects( frame, rects );
         rects.clear();
         player << frame;
